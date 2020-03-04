edf1b9b470f2467c371735033fb7b2a918855a19
Damien <mail@damientimewell.com>
2020-03-04 12:11:40 +0100

    Using cloudinary for image uploads

--- server.js
+++ server.js
@@ -1,24 +1,56 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import dotenv from 'dotenv'
+import cloudinary from 'cloudinary'
+import multer from 'multer'
+import cloudinaryStorage from 'multer-storage-cloudinary'
+import mongoose from 'mongoose'
 
-// Defines the port the app will run on. Defaults to 8080, but can be 
-// overridden when starting the server. For example:
-//
-//   PORT=9000 npm start
+dotenv.config()
+
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/imageUploadsCodealong"
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.Promise = Promise
+
+const Pet = mongoose.model('Pet', {
+  name: String,
+  imageUrl: String,
+  imageId: String
+})
+
+cloudinary.config({
+  cloud_name: 'dpem2z8y9',
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET
+})
+
+const storage = cloudinaryStorage({
+  cloudinary: cloudinary,
+  folder: 'pets',
+  allowedFormats: ["jpg", "png"],
+  transformation: [{ width: 500, height: 500, crop: "limit" }]
+})
+const parser = multer({ storage: storage })
 const port = process.env.PORT || 8080
 const app = express()
 
-// Add middlewares to enable cors and json body parsing
 app.use(cors())
 app.use(bodyParser.json())
 
-// Start defining your routes here
 app.get('/', (req, res) => {
   res.send('Hello world')
 })
 
-// Start the server
+app.post('/pets', parser.single('image'), async (req, res) => {
+  try {
+    const pet = await new Pet({ name: req.body.name, imageUrl: req.file.url, imageId: req.file.public_id }).save()
+    res.json(pet)
+  } catch (err) {
+    res.status(400).json({ errors: err.errors })
+  }
+})
+
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`)
 })
